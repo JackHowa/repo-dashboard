33c329e5b29651a54a42289b0f671a5ad637170c
Jack Howard <jackhowaa@gmail.com>
2019-04-04 16:47:28 -0500

    Add votes api local enum

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -1,3 +1,4 @@
 export default {
-  GITHUB_REPOS_API: 'https://api.github.com/repos/'
+  GITHUB_REPOS_API: 'https://api.github.com/repos/',
+  VOTE_COUNTER_API: 'http://localhost:3001/api/votes'
 };
