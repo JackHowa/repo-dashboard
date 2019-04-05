e0c6e950b06188ec3913f96b5bc0663535f32769
Jack Howard <jackhowaa@gmail.com>
2019-04-05 08:49:04 -0500

    Add refresh rate constant

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -1,5 +1,5 @@
 export default {
   GITHUB_REPOS_API: 'https://api.github.com/repos/',
-  VOTE_COUNTER_API: 'https://enigmatic-hollows-17650.herokuapp.com/api/votes'
-  // VOTE_COUNTER_API: 'http://localhost:3001/api/votes'
+  VOTE_COUNTER_API: 'https://enigmatic-hollows-17650.herokuapp.com/api/votes',
+  REFRESH_RATE: 1000
 };
